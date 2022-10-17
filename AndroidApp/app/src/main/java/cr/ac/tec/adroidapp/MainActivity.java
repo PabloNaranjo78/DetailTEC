a7f1b4787934716d3e26648385aa540cc9a4cf7e
FMonge <fmonge13@estudiantec.cr>
2022-10-17 17:48:02 -0600

    Login GUI

--- AndroidApp/app/src/main/java/cr/ac/tec/adroidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/cr/ac/tec/adroidapp/MainActivity.java
@@ -1,36 +1,97 @@
 package cr.ac.tec.adroidapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import java.io.Console;
 import java.util.List;
+import java.util.Objects;
+
+import cr.ac.tec.adroidapp.gui.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView userText;
+    TextView passwordText;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue_gray)));
 
         DataBase auxdb = Room.databaseBuilder(getApplicationContext(), DataBase.class, "prueba1").allowMainThreadQueries().build();
-        //auxdb.daoProject().insertCliente(new Cliente(123, "Monge", "fff", "SepaDios", "Fernando", "monge@123", 5));
+
+        userText = findViewById(R.id.pt_Usuario);
+        passwordText = findViewById(R.id.pt_Contraseña);
+        loginButton = findViewById(R.id.btn_Login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userValidation(userText.getText().toString(), passwordText.getText().toString())){
+                    Intent intent = new Intent(getApplicationContext(), Menu.class);
+                    startActivity(intent);
+                }
+                else{
+                    AlertDialog alertMessage = new AlertDialog.Builder(MainActivity.this).create();
+                    alertMessage.setTitle("Error");
+                    alertMessage.setMessage("Credenciales incorrectas");
+                    alertMessage.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                    alertMessage.show();
+                }
+
+            }
+        });
+
+
+
+
+        //Mongeauxdb.daoProject().insertCliente(new Cliente(123, "Monge", "fff", "SepaDios", "Fernando", "monge@123", 5));
         //auxdb.daoProject().insertClienteTelefonos(new ClienteTelefonos(123, 50025063) );
 //        auxdb.daoProject().insertClienteTelefonos(new ClienteTelefonos(123, 89215510) );
 
-        List<ClienteTelefonos> listaux = auxdb.daoProject().getClienteTelefonos();
-        String texux = "";
 
-        for (int i =0; i < listaux.size(); i++){
-            texux = texux + "ID: " + listaux.get(i).IDCliente + " Telefono: " + listaux.get(i).Telefono + "\n";
 
-        }
+//        List<ClienteTelefonos> listaux = auxdb.daoProject().getClienteTelefonos();
+//        String texux = "";
+//
+//        for (int i =0; i < listaux.size(); i++){
+//            texux = texux + "ID: " + listaux.get(i).IDCliente + " Telefono: " + listaux.get(i).Telefono + "\n";
+//
+//        }
+//
+//
+//        System.out.println(texux);
 
-        System.out.println("AAAAAAAAAAA");
-        System.out.println(texux);
 
+
+    }
+    public boolean userValidation(String user, String pass){
+        DataBase dataBase = Room.databaseBuilder(getApplicationContext(), DataBase.class, "prueba1").allowMainThreadQueries().build();
+        List<Cliente> clientList = dataBase.daoProject().getClientes();
+            for (int i = 0; i < clientList.size(); i++){
+                if (Objects.equals(clientList.get(i).Usuario, user) & Objects.equals(clientList.get(i).Contraseña, pass)){
+                    return true;
+                }
+            }
+            return false;
     }
 
 
---
+++ AndroidApp/app/src/main/java/cr/ac/tec/adroidapp/gui/Puntos.java
@@ -0,0 +1,16 @@
+package cr.ac.tec.adroidapp.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cr.ac.tec.adroidapp.R;
+
+public class Puntos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntos);
+    }
+}
